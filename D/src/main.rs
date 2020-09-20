669834eb78758df8d05d10d35f20d3872309f428
Jake Fecher <jfecher11@gmail.com>
2020-09-20 16:48:58 -0400

    Simplify is_mouse_in_hexagon

--- D/src/main.rs
+++ D/src/main.rs
@@ -1,50 +1,40 @@
 use gio::prelude::*;
 use gtk::prelude::*;
 use gtk::DrawingArea;
-
-use cairo::Context;
 use std::cmp::max;
 
 const LEFT_CLICK: u32 = 1;
 
-type Hexagon = Vec<(f64, f64)>;
+#[derive(Clone)]
+struct Hexagon(pub Vec<(f64, f64)>);
 
-fn build_ui(application: &gtk::Application, hexagon: Hexagon) {
-    draw(application, hexagon.clone(), move |_, cr| {
-        cr.set_source_rgb(1.0, 0.0, 0.0);
-
-        for (x, y) in hexagon.iter().copied() {
-            cr.line_to(x, y);
-        }
-
-        cr.fill();
-        Inhibit(false)
-    });
+impl Hexagon {
+    fn get_size(&self) -> (i32, i32) {
+        self.0.iter().fold((0, 0), |acc, element| {
+            (max(acc.0, element.0 as i32), max(acc.1, element.1 as i32))
+        })
+    }
 }
 
-fn bounds_check(points: &Hexagon, x: f64, y: f64) -> bool {
-    let xs = points.iter().map(|&(x, _)| x).collect::<Vec<_>>();
-    let ys = points.iter().map(|&(_, y)| y).collect::<Vec<_>>();
+fn is_mouse_in_hexagon(hexagon: &Hexagon, x: f64, y: f64) -> bool {
+    let (_, height) = hexagon.get_size();
+    let size = height as f64 / 2.0;
 
-    let mut c = false;
-    let indices = [5, 0, 1, 2, 3, 4, 5];
+    // booleans representing different "inside of hexagon" cases
+    let bot_left = y <= size + x;
+    let top_left = y >= size - x;
+    let bot_right = y <= 4.0 * size - x;
+    let top_right = y >= -2.0 * size + x;
+    let vertical_bounds = (y <= 2.0 * size) && (y >= 0.0);
 
-    for index in 1..indices.len() {
-        let (i, j) = (indices[index], indices[index - 1]);
-
-        if ((ys[i] > y) != (ys[j] > y)) && (x < (xs[j] - xs[i]) * (y - ys[i]) / (ys[j] - ys[i]) + xs[i]) {
-            c = !c;
-        }
-    }
-
-    c
+    bot_left && top_left && bot_right && top_right && vertical_bounds
 }
 
 fn on_click(hexagon: Hexagon) -> impl Fn(&gtk::ApplicationWindow, &gdk::EventButton) -> Inhibit {
     move |window, event| {
         if event.get_event_type() == gdk::EventType::ButtonPress && event.get_button() == LEFT_CLICK {
             let (x, y) = event.get_coords().unwrap();
-            if bounds_check(&hexagon, x, y) {
+            if is_mouse_in_hexagon(&hexagon, x, y) {
                 window.destroy();
             }
         }
@@ -59,36 +49,37 @@ fn main() {
     let scale = std::env::args().nth(1).unwrap()
         .parse::<u64>().unwrap() as f64;
 
-    let points: Hexagon = [
+    let hexagon = Hexagon([
         (0.0,  1.0), (1.0,  2.0), (2.0,  2.0),
         (3.0,  1.0), (2.0,  0.0), (1.0,  0.0),
-    ].iter().map(|&(x, y)| (x * scale, y * scale)).collect(); // TODO make function
+    ].iter().map(|&(x, y)| (x * scale, y * scale)).collect()); // TODO make function
 
     application.connect_activate(move |app| {
-        build_ui(app, points.clone());
+        build_ui(app, hexagon.clone());
     });
 
     application.run(&[]);
 }
 
-fn draw<F>(application: &gtk::Application, hexagon: Hexagon, draw_fn: F)
-where
-    F: Fn(&DrawingArea, &Context) -> Inhibit + 'static,
-{
+fn build_ui(application: &gtk::Application, hexagon: Hexagon) {
     let drawing_area = DrawingArea::new();
-    drawing_area.connect_draw(draw_fn);
+
+    let hexagon_copy = hexagon.clone();
+    drawing_area.connect_draw(move |_, context| {
+        context.set_source_rgb(1.0, 0.0, 0.0);
+
+        for (x, y) in hexagon_copy.0.iter().copied() {
+            context.line_to(x, y);
+        }
+
+        context.fill();
+        Inhibit(false)
+    });
 
     let window = gtk::ApplicationWindow::new(application);
-    let (width, height) = get_size(&hexagon);
+    let (width, height) = hexagon.get_size();
     window.set_default_size(width, height);
     window.add(&drawing_area);
     window.connect_button_press_event(on_click(hexagon));
     window.show_all();
-}
-
-// Gets width and height of hexagon
-fn get_size(h: &Hexagon) -> (i32, i32) {
-    h.iter().fold((0, 0), |acc, element| {
-        (max(acc.0, element.0 as i32), max(acc.1, element.1 as i32))
-    })
 }
