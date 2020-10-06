edcee83de9c0f0c2fc64267b71afd2085b1d06bc
Jake Fecher <jfecher11@gmail.com>
2020-10-05 20:46:49 -0400

    Get basic fish drawing

--- Fish/Common/src/client/mod.rs
+++ Fish/Common/src/client/mod.rs
@@ -1,11 +1,12 @@
 use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{Image, DrawingArea};
+use gtk::{ Image, Fixed };
 use std::cmp::max;
 use crate::common::tile::Tile;
 
 const LEFT_CLICK: u32 = 1;
-const IMAGE_FILENAME: &str = "../../assets/fish.png";
+const FISH_FILENAME_TEMPLATE: &str = "assets/fish";
+const HEXAGON_FILENAME: &str = "assets/hexagon.png";
 
 #[derive(Clone)]
 struct Hexagon(pub [(f64, f64); 6]);
@@ -38,36 +39,40 @@ impl Hexagon {
     }
 }
 
-fn make_fish_image() -> Image {
-    Image::new_from_file(IMAGE_FILENAME)
+fn make_fish_image(fish_count: u8) -> Image {
+    let filename = format!("{}{}.png", FISH_FILENAME_TEMPLATE, fish_count);
+    Image::new_from_file(filename)
 }
 
 /// Generates a GTK drawing of a specific hexagon
-fn make_hexagon_drawing(hexagon: Hexagon) -> DrawingArea {
-    let drawing_area = DrawingArea::new();
+fn make_hexagon_layout(hexagon: Hexagon) -> gtk::Fixed {
+    let layout = gtk::Fixed::new();
+    let hexagon = Image::new_from_file(HEXAGON_FILENAME);
 
-    // draw lines (edges) between hexagon vertices, and color inside edges
-    drawing_area.connect_draw(move |_, context| {
-        context.set_source_rgb(1.0, 0.0, 0.0);
+    let fish = make_fish_image(2);
+    layout.add(&hexagon);
+    layout.add(&fish);
 
-        for (x, y) in hexagon.0.iter().copied() {
-            context.line_to(x, y);
-        }
+    // TODO: Remove constants
+    // Both hexagon.get_allocated() and hexagon.size_request are (0, 0) for some reason.
+    let hexagon_size = (300, 200);
+    let fish_size = (71, 63);
 
-        context.fill();
-        Inhibit(false)
-    });
-    
-    drawing_area
+    // Center the fish on the hexagon
+    layout.move_(&fish,
+        hexagon_size.0 / 2 - fish_size.0 / 2,
+        hexagon_size.1 / 2 - fish_size.1 / 2);
+
+    layout
 }
 
 /// Makes and shows a window in a given application displaying the given hexagon
 fn make_window(application: &gtk::Application, hexagon: Hexagon) {
     let window = gtk::ApplicationWindow::new(application);
-    let drawing_area = make_hexagon_drawing(hexagon.clone());
+    let layout = make_hexagon_layout(hexagon.clone());
     let (width, height) = hexagon.dimensions();
     window.set_default_size(width, height);
-    window.add(&drawing_area);
+    window.add(&layout);
     window.show_all();
 }
 
