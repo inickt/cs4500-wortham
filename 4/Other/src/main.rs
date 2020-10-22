fb7a36a04ee1faf0c4a70a490cca6bf6fca015b4
Ryan Drew <drew.r@northeastern.edu>
2020-10-21 21:56:41 -0400

    Serialize xstate

--- 4/Other/src/main.rs
+++ 4/Other/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 use serde_json::Deserializer;
 use serde::{ Serialize, Deserialize };
@@ -8,26 +8,29 @@ use fish::common::boardposn::BoardPosn;
 use fish::common::direction::{ Direction, Direction::* };
 use fish::common::gamestate::GameState;
 use fish::common::penguin::PenguinId;
+use fish::common::player::{ Player, PlayerId };
 
 #[derive(Serialize, Deserialize)]
 struct JSONPlayersAndBoard {
     players: Vec<JSONPlayer>,
-    board: Vec<Vec<u32>>, // vector of rows
+    board: JSONBoard,
 }
+
+type JSONBoard = Vec<Vec<u32>>;
 
 #[derive(Serialize, Deserialize)]
 struct JSONPlayer {
-    _color: JSONColor,
-    _score: usize, // do we need arbitrary precision? 4 says "Natural"
+    color: JSONColor,
+    score: usize, // do we need arbitrary precision? 4 says "Natural"
     places: Vec<JSONPosition>
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Copy, Clone, Serialize, Deserialize)]
 enum JSONColor {
-    Red,
-    White,
-    Brown,
-    Black,
+    red,
+    white,
+    brown,
+    black,
 }
 
 /// Json pair of [ board_row, board_column ]
@@ -45,7 +48,7 @@ impl JSONPlayersAndBoard {
 
 /// Converts a JSON representation of a board to
 /// the board module's Board representation.
-fn board_from_json(board: &[Vec<u32>]) -> Board {
+fn board_from_json(board: &JSONBoard) -> Board {
     let rows = board.len();
     let columns = board.iter().map(|row| row.len()).max().unwrap_or(0);
     let mut holes = Vec::new();
@@ -67,10 +70,26 @@ fn board_from_json(board: &[Vec<u32>]) -> Board {
     Board::with_holes(rows as u32, columns as u32, holes, 0)
 }
 
+fn make_color_mapping(gamestate: &GameState, json_players: &[JSONPlayer]) -> HashMap<PlayerId, JSONColor> {
+    gamestate.turn_order.iter().copied()
+        .zip(json_players.iter().map(|json_player| json_player.color))
+        .collect()
+}
+
+fn set_player_scores(gamestate: &mut GameState, json_players: &[JSONPlayer]) {
+    assert_eq!(gamestate.turn_order.len(), json_players.len());
+
+    for (turn, json_player) in gamestate.turn_order.iter().copied().zip(json_players.iter()) {
+        let player = gamestate.players.get_mut(&turn).unwrap();
+        player.score = json_player.score;
+    }
+}
+
 fn place_penguins(gamestate: &mut GameState, json_players: &[JSONPlayer]) {
     let all_penguins: Vec<Vec<_>> =
-        gamestate.players.iter().map(|(&player_id, player)| {
-            player.penguins.iter().map(move |penguin| (player_id, penguin.penguin_id)).collect()
+        gamestate.turn_order.iter().map(|player_id| {
+            let player = &gamestate.players[player_id];
+            player.penguins.iter().map(move |penguin| (*player_id, penguin.penguin_id)).collect()
         }).collect();
 
     // This just completely ignores the player colors and just assigns each player their
@@ -78,8 +97,8 @@ fn place_penguins(gamestate: &mut GameState, json_players: &[JSONPlayer]) {
     for (penguin_ids, json_player) in all_penguins.into_iter().zip(json_players.iter()) {
         assert_eq!(penguin_ids.len(), json_player.places.len());
         for ((player_id, penguin_id), place) in penguin_ids.into_iter().zip(json_player.places.iter()) {
-            let tile_id = gamestate.board.get_tile(place[0], place[1]).unwrap().tile_id;
-            gamestate.place_avatar_for_player(player_id, penguin_id, tile_id);
+            let tile_id = gamestate.board.get_tile(place[1], place[0]).unwrap().tile_id;
+            gamestate.place_avatar_without_changing_turn(player_id, penguin_id, tile_id);
         }
     }
 }
@@ -102,8 +121,52 @@ fn try_move_penguin(gamestate: &mut GameState, penguin_id: PenguinId, direction:
     }
 }
 
-fn serialize_gamestate(gamestate: &GameState) -> JSONPlayersAndBoard {
-    unimplemented!()
+fn serialize_board(board: &Board) -> JSONBoard {
+    let mut rows = vec![];
+
+    for row_i in 0 .. board.height {
+        let mut new_row = vec![];
+        for col_i in 0 .. board.width {
+            let tile = board.get_tile(col_i, row_i);
+            let fish_count = tile.map_or(0, |tile| tile.fish_count);
+            new_row.push(fish_count);
+        }
+    }
+
+    rows
+}
+
+fn serialize_player(player: &Player, board: &Board, color_mapping: &HashMap<PlayerId, JSONColor>) -> JSONPlayer {
+    let mut places = vec![];
+
+    for penguin in player.penguins.iter() {
+        let tile_id = penguin.tile_id.unwrap(); // Input should contain only placed penguins, therefore all tile_ids should be Some(id)
+        let position = board.get_tile_position(tile_id);
+        places.push([position.y, position.x]);
+    }
+
+    JSONPlayer {
+        color: color_mapping[&player.player_id],
+        score: player.score,
+        places
+    }
+}
+
+fn serialize_players(gamestate: &GameState, color_mapping: &HashMap<PlayerId, JSONColor>) -> Vec<JSONPlayer> {
+    let mut players_json = vec![];
+    for id in gamestate.turn_order.iter() {
+        let player = &gamestate.players[id];
+        players_json.push(serialize_player(player, &gamestate.board, color_mapping));
+    }
+
+    players_json
+}
+
+fn serialize_gamestate(gamestate: &GameState, color_mapping: &HashMap<PlayerId, JSONColor>) -> JSONPlayersAndBoard {
+    let board = serialize_board(&gamestate.board);
+    let players = serialize_players(gamestate, color_mapping);
+
+    JSONPlayersAndBoard { players, board }
 }
 
 fn main() {
@@ -114,13 +177,16 @@ fn main() {
     let gamestate = GameState::new(0, board, json.players.len());
     let mut gamestate = gamestate.borrow_mut();
 
+    let player_color_mapping = make_color_mapping(&gamestate, &json.players);
+
+    set_player_scores(&mut gamestate, &json.players);
     place_penguins(&mut gamestate, &json.players);
 
     let first_penguin = gamestate.current_player().penguins[0].penguin_id;
 
     for direction in &[North, Northeast, Southeast, South, Southwest, Northwest] {
         if try_move_penguin(&mut gamestate, first_penguin, *direction) {
-            let state = serialize_gamestate(&gamestate);
+            let state = serialize_gamestate(&gamestate, &player_color_mapping);
             println!("{}", serde_json::to_string(&state).unwrap());
         }
     }
