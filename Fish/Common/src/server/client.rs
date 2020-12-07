642ef7f2a1981382e871d79b57ede2a7f627ff32
Nick Thompson <git@nickt.dev>
2020-12-06 23:24:02 -0500

    Documentation on Client trait

--- Fish/Common/src/server/client.rs
+++ Fish/Common/src/server/client.rs
@@ -5,13 +5,20 @@ use crate::common::player::{ PlayerColor, PlayerId };
 use std::cell::RefCell;
 use std::rc::Rc;
 
+/// Represents a Client that can interact with a Fish tournament and play in Fish games.
+/// All functions that return None will result in the Client in being kicked from the game.
+/// Placements and movements will only be requested from Clients when the respective action is possible.
 pub trait Client {
-    // TODO documentation
+    /// Called when a tournament is starting
     fn tournament_starting(&mut self) -> Option<()>;
+    /// Called when a tournament is ending, with this client's result whether they won or lost
     fn tournament_ending(&mut self, won: bool) -> Option<()>;
 
+    /// Called when a game is starting, with the initial game state and the color this client is playing as
     fn initialize_game(&mut self, initial_gamestate: &GameState, player_color: PlayerColor) -> Option<()>;
+    /// Gets a penguin placement from a client
     fn get_placement(&mut self, gamestate: &GameState) -> Option<Placement>;
+    /// Gets a move from a client
     fn get_move(&mut self, gamestate: &GameState, previous: &[PlayerMove]) -> Option<Move>;
 }
 
